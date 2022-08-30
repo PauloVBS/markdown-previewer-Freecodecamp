37a7daf3eb06441a20f9faba7b96c6dca495cf2b
slasher314 <paulo.vb.souza@unesp.br>
2022-08-30 17:29:31 -0300

    Finished

--- src/App.js
+++ src/App.js
@@ -3,69 +3,50 @@ import React from 'react'
 import {marked} from 'marked'
 
 function App() {
-  const defaultContent=`a
-  # Welcome to my React Markdown Previewer!
-
-  ## This is a sub-heading...
-  ### And here's some other cool stuff:
-  
-  Heres some code, \`<div></div>\`, between 2 backticks.
-  
+  marked.setOptions({
+    breaks:true,
+    renderer: new marked.Renderer()
+  })
+  const defaultContent=`
+  # a header (H1 size)
+  ## a sub header (H2 size)
+  a link [github Link](https://github.com/PauloVBS)
+  \`inline code \`
   \`\`\`
-  // this is multi-line code:
-  
-  function anotherExample(firstLine, lastLine) {
-    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-      return multiLineCode;
-    }
+  function HelloWorld(){
+  console.log("Hello World!");
   }
   \`\`\`
-  
-  You can also make text **bold**... whoa!
-  Or _italic_.
-  Or... wait for it... **_both!_**
-  And feel free to go crazy ~~crossing stuff out~~.
-  
-  There's also [links](https://www.freecodecamp.org), and
-  > Block Quotes!
-  
-  And if you want to get really crazy, even tables:
-  
-  Wild Header | Crazy Header | Another Header?
-  ------------ | ------------- | -------------
-  Your content can | be here, and it | can be here....
-  And here. | Okay. | I think we get it.
-  
-  - And of course there are lists.
-    - Some are bulleted.
-       - With different indentation levels.
-          - That look like this.
-  
-  
-  1. And there are numbered lists too.
-  1. Use just 1s if you want!
-  1. And last but not least, let's not forget embedded images:
-  
-  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  * a list item
+  > a blockquote
+
+  an image 
+  ![Vs Code logo](https://code.visualstudio.com/assets/images/code-stable.png)
+  **and bolded text.**
   `
   const [content, setContent] = React.useState({
     preview:  defaultContent,
     editorContent:""
   });
   
-  function Parser(props){
-    return (<div dangerouslySetInnerHTML={{__html: marked(props.text,{breaks:true})}} id ="preview" ></div>)
-  };
 
   function handleChangeEditor(event){
     setContent({...content,preview: event.target.value})
   }
   return (
     <div className="App">
-      <textarea id="editor" onChange={handleChangeEditor} type="text" value={content.preview} />
+      <div id="editor-window"><Header title="Editor"/><textarea id="editor" onChange={handleChangeEditor} type="text" value={content.preview} />
+      </div>
       <Parser text={content.preview}/>
     </div>
   );
+}
+function Parser(props){
+  return (<div id="previewer"><Header title="Preview"/><div dangerouslySetInnerHTML={{__html: marked.parse(props.text)}} id ="preview" >
+  </div></div>)
+};
+function Header(props){
+  return (<header id='headerer'>{props.title}</header>)
 }
 
 export default App;
